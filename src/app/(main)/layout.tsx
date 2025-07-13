14f4484f1b0f97dc3f27a69b586da94445164cdc
ochieng clement <odokclement@gmail.com>
2025-07-13 22:16:04 +0300

    pagination updates

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -4,12 +4,15 @@ import Sidebar from "@/components/Sidebar";
 
 const MainLayout = ({children}:{ children: React.ReactNode}) => {
     return ( <>
-        <Navbar />
+        <div className="fixed top-0 left-0 right-0 z-10 bg-white shadow-md">
+            <Navbar />
+        </div>
+        
           <div className="flex">
             <div className="hidden md:block h-[100vh] w-[300px] ">
               <Sidebar />
             </div>
-            <div className=" p-5 w-full md:max-w-[1140px]">{children}</div>
+            <div className=" p-5 mt-10 w-full md:max-w-[1140px]">{children}</div>
           </div>
     </> );
 }
